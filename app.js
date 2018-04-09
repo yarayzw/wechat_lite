40c23ada4a6c075aa315de3bb8522e0f689dab01
shawn <csk_shawn@163.com>
2018-04-10 00:34:51 +0800

    1.支付域名

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ App({
     userInfo: null,
     host: 'http://211.149.177.232/DataCenterService.asmx/',
     php_host: 'http://211.149.177.232:8008/wechat.php/',
+    pay_host: 'http://shawn.wechat/index.php/home/paywechat/',
     user_phone: '',
     wx_code: '',
     add_phone: '',
