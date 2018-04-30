9165bd0c70fd6d9bbf9cd2ded3c15d7c82814eb1
shawn <csk_shawn@163.com>
2018-05-01 00:38:07 +0800

    1.更新

--- pages/userInfo/userInfo.js
+++ pages/userInfo/userInfo.js
@@ -86,5 +86,35 @@ Page({
     wx.redirectTo({
       url: '/pages/login/login',
     })
+  },
+  gengxin: function () {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+      if (!res.hasUpdate) {
+        wx.showToast({
+          title: '已是最新版本',
+          icon: 'loading',
+          duration: 1500,
+          mask: true
+        })
+      } else {
+        updateManager.onUpdateReady(function () {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function (res) {
+              if (res.confirm) {
+                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                updateManager.applyUpdate()
+              }
+            }
+          })
+
+        })
+      }
+    })
   }
 })
